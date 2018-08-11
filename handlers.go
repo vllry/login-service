8e3f673e053691943152559d1de19945e3d71ee7
vallery <vallerylancey@gmail.com>
2018-08-11 10:20:20 -0700

    Fixed trace submission, added a span.

--- handlers.go
+++ handlers.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"github.com/gorilla/mux"
+	"github.com/opentracing/opentracing-go"
 	"net/http"
 )
 
@@ -12,6 +13,9 @@ func IndexHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func LoginHandler(w http.ResponseWriter, r *http.Request) {
+	span := opentracing.GlobalTracer().StartSpan("login-api-call")
+	defer span.Finish()
+
 	vars := mux.Vars(r)
 
 	username := vars["userId"]
@@ -19,8 +23,10 @@ func LoginHandler(w http.ResponseWriter, r *http.Request) {
 
 	// TODO
 	if username == "test" && secretPassword == "test" {
+		span.LogKV("event", "authentication success")
 		token, err := generateToken(username)
 		if err != nil {
+			span.LogKV("event", "error getting token", "error", err)
 			w.WriteHeader(http.StatusInternalServerError)
 			fmt.Println(err)
 			fmt.Println("Error generating token.")
--- main.go
+++ main.go
@@ -1,7 +1,34 @@
 package main
 
+import (
+	"github.com/opentracing/opentracing-go"
+	"github.com/uber/jaeger-client-go"
+	"github.com/uber/jaeger-client-go/config"
+	"time"
+)
+
 func main() {
-	initializeTracing()
+	cfg := config.Configuration{
+		Sampler: &config.SamplerConfig{
+			Type:  "const",
+			Param: 1,
+		},
+		Reporter: &config.ReporterConfig{
+			LogSpans:            true,
+			LocalAgentHostPort: "jaeger:6832",
+			BufferFlushInterval: 1 * time.Second,
+		},
+	}
+	tracer, closer, err := cfg.New(
+		"login",
+		config.Logger(jaeger.StdLogger),
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	opentracing.SetGlobalTracer(tracer)
+	defer closer.Close()
 
 	s := newServer()
 	s.start()
